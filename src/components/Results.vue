fe9ec98d4800ed182768fe16f2dec2bedf85270d
mazin <mazink2@gmail.com>
2023-10-26 05:32:20 -0400

    Added rank to Results component, added styles

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,9 +1,32 @@
 <template>
   <p>Reaction Time: {{ score }} ms</p>
+  <div class="rank">{{ rank }}</div>
 </template>
 
 <script>
 export default {
   props: ["score"],
+  data() {
+    return {
+      rank: null
+    }
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = 'Super Fast'
+    } else if (this.score < 400) {
+      this.rank = 'Fast'
+    } else {
+      this.rank = 'Slow af'
+    }
+  }
 };
 </script>
+
+<style>
+  .rank {
+    font-size: 1.4em;
+    color: #0faf87;
+    font-weight: bold;
+  }
+</style>
